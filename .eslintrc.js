0287e8444909c38cde44672acd4e0bc63a71aeb3
Anuj Nair <anair@slack-corp.com>
2018-05-19 22:22:07 -0700

    ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  env: {
+    es6: true,
+    node: true
+  },
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }]
+  },
+  globals: {
+    document: true
+  }
+};
